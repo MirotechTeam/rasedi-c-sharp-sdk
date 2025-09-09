ab37249ab7b7c7f348e20dd574f72be2e762acc2
Mohamed Gharib <mohamed.9.gharib@gmail.com>
2025-09-09 16:52:33 +0300

    #Adding a class for testing the sdk

---
+++ Test/TestSdk.cs
@@ -0,0 +1,40 @@
+﻿using MiroPaySDK.Contracts;
+using MiroPaySDK.Rest;
+using MiroPaySDK.Rest.Enums;
+using System;
+
+namespace MiroPay.Test
+{
+    static class TestSdk
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var client = new PaymentRestClient("Your private key", "Your secret key");
+
+
+
+            var request = new CreatePayment
+            {
+                Amount = "2500",
+                Gateways = new[] { GateWays.FIB, GateWays.ZAIN },
+                Title = "Title ...",
+                Description = "Description ...",
+                RedirectUrl = "http://www.yoursite.com/sample",
+                CollectFeeFromCustomer = true,
+                CollectCustomerEmail = true,
+                CollectCustomerPhoneNumber = true,
+            };
+
+            var response = await client.CreatePaymentAsync(request);
+
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Reference Code: {response.Body?.ReferenceCode}");
+            Console.WriteLine($"Amount: {response.Body?.Amount}");
+            Console.WriteLine($"Status: {response.Body?.Status}");
+            Console.WriteLine($"Redirect URL: {response.Body?.RedirectUrl}");
+            Console.WriteLine($"Paid At: {response.Body?.PaidAt}");
+            Console.WriteLine($"Paid Via: {response.Body?.PaidVia}");
+        }
+    }
+}
